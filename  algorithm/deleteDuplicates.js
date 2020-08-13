5087ebf547e6d10e23e8baf7545718174d5c2f4e
Pearyman <pearyman@gmail.com>
2020-08-13 12:17:39 +0800

    feat:  #83

---
+++  algorithm/deleteDuplicates.js
@@ -0,0 +1,16 @@
+// #83
+
+var deleteDuplicates = function(head){
+  let p = head
+ 
+  while(p && p.next){
+    if(p.val === p.next.val){
+      p.next = p.next.next
+    }else{
+      p = p.next
+    }
+  }
+  return head
+}
+
+deleteDuplicates([1,1,2])
