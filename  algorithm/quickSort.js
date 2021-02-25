842d7ccd7e5ca2f25ac9acdd64d9cf18915843c5
Pearyman <pearyman@gmail.com>
2021-02-25 17:46:11 +0800

    feat: sort

---  algorithm/quickSort.js
+++  algorithm/quickSort.js
@@ -3,23 +3,23 @@
  * @param {quickSort} arr 
  */
 
-// const quickSort = (arr) =>{
-//   if(arr.length <= 1) {return arr;}
-//   let left = []
-//   let right = []
-//   let mid = arr[0]
-//   for(let i =1 ;i<arr.length; i++){
-//     if(arr[i]>mid){
-//       right.push(arr[i])
-//     }else{
-//       left.push(arr[i])
-//     }  
-//   }
-//   return [...quickSort(left), mid, ...quickSort(right)]
-// }
+const quickSort = (arr) => {
+  if (arr.length <= 1) { return arr; }
+  let left = []
+  let right = []
+  let mid = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > mid) {
+      right.push(arr[i])
+    } else {
+      left.push(arr[i])
+    }
+  }
+  return [...quickSort(left), mid, ...quickSort(right)]
+}
 
-// const result = quickSort([3,66,55,44,1,88])
-// console.log(result);
+const result = quickSort([3, 66, 55, 44, 1, 88])
+console.log(result)
 
 
 
@@ -27,9 +27,9 @@
  * 冒泡排序
  */
 
- const bubbleSort = (arr) => {
+// const bubbleSort = (arr) => {
 
- }
+// }
 
- const result = bubbleSort([3,66,55,44,1,88])
-console.log(result);
+// const result = bubbleSort([3, 66, 55, 44, 1, 88])
+// console.log(result);
