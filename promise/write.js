c8da9c124cfc6fe29286eb6afd0fa9f6bcea2565
Pearyman <pearyman@gmail.com>
2021-03-07 22:57:34 +0800

    feat: 添加html文件夹

--- promise/write.js
+++ promise/write.js
@@ -24,32 +24,102 @@
 /**
  * 红灯 3S  绿灯 2s  蓝灯 1s 交替
  */
-function red() {
-  console.log('red')
-}
-function green() {
-  console.log('green')
-}
-function blue() {
-  console.log('blue')
-}
+// function red() {
+//   console.log('red')
+// }
+// function green() {
+//   console.log('green')
+// }
+// function blue() {
+//   console.log('blue')
+// }
 
-const task = (timer, name) => {
-  setTimeout(() => {
-    if (name === 'red') {
-      red()
-    } else if (name === 'green') {
-      green()
-    } else if (name === 'blue') {
-      blue()
+// const task = (timer, name) => {
+//   setTimeout(() => {
+//     if (name === 'red') {
+//       red()
+//     } else if (name === 'green') {
+//       green()
+//     } else if (name === 'blue') {
+//       blue()
+//     }
+//   }, timer);
+// }
+
+// const step = async () => {
+//   await task(3000, 'red')
+//   await task(2000, 'green')
+//   await task(1000, 'blue')
+//   step()
+// }
+// step()
+
+
+/**
+ * 防抖
+ */
+
+function debounce(fn, delay) {
+  let timer = null
+  return function () {
+    if (timer) {
+      clearTimeout(timer)
     }
-  }, timer);
+    timer = setTimeout(fn, delay)
+  }
 }
 
-const step = async () => {
-  await task(3000, 'red')
-  await task(2000, 'green')
-  await task(1000, 'blue')
-  step()
+/**
+ * 节流 
+ * @param {*} fn 
+ * @param {*} delay 
+ */
+function throttle(fn, delay) {
+  let valid = true
+  if (!valid) {
+    return false
+  }
+
+  valid = false
+  setTimeout(() => {
+    fn()
+    valid = true
+  }, delay);
 }
-step()
+
+
+
+console.log(1);
+setTimeout(() => {
+  console.log(2);
+  process.nextTick(() => {
+    console.log(3);
+  });
+  new Promise((resolve) => {
+    console.log(4);
+    resolve();
+  }).then(() => {
+    console.log(5);
+  });
+});
+new Promise((resolve) => {
+  console.log(7);
+  resolve();
+}).then(() => {
+  console.log(8);
+});
+process.nextTick(() => {
+  console.log(6);
+});
+setTimeout(() => {
+  console.log(9);
+  process.nextTick(() => {
+    console.log(10);
+  });
+  new Promise((resolve) => {
+    console.log(11);
+    resolve();
+  }).then(() => {
+    console.log(12);
+  });
+});
